cf6a49f462875429781e3ffecbd80dc3b4e91390
Yatheendra Pravan <pravan.yatheen@gmail.com>
2022-04-18 23:05:35 +0100

    fixed few bugs

--- script.py
+++ script.py
@@ -20,16 +20,16 @@ else:
 
 #STEP 0: SYNC TWEETS DATABASES and FILTER OUT TWEETS WITHOUT EMOTICONS
 target_emoji_list = [
-    '0x1F602', '0x1F60D', '0x1F62D', '0x1F618', '0x1F60A', '0x1F601', '0x1F629', '0x1F64F',
-    '0x1F60F', '0x1F609', '0x1F64C', '0x1F648', '0x1F604', '0x1F612', '0x1F603', '0x1F614',
-    '0x1F631', '0x1F61C', '0x1F633', '0x1F621', '0x1F60E', '0x1F622', '0x1F60B', '0x1F64A',
-    '0x1F634', '0x1F60C', '0x1F61E', '0x1F606', '0x1F61D', '0x1F62A', '0x1F62B', '0x1F605',
-    '0x1F600', '0x1F61A', '0x1F63B', '0x1F625', '0x1F615', '0x1F624', '0x1F608', '0x1F630',
-    '0x1F611', '0x1F639', '0x1F620', '0x1F613', '0x1F623', '0x1F610', '0x1F628', '0x1F616', 
-    '0x1F637', '0x1F64B', '0x1F61B', '0x1F62C', '0x1F619', '0x1F646', '0x1F645', '0x1F649',
-    '0x1F607', '0x1F63F', '0x1F632', '0x1F636', '0x1F635', '0x1F638', '0x1F627', '0x1F62E',
-    '0x1F63D', '0x1F640', '0x1F647', '0x1F61F', '0x1F62F', '0x1F626', '0x1F64D', '0x1F63A',
-    '0x1F63E', '0x1F63C', '0x1F64E', '0x1F617'
+    '0x1f602', '0x1f60d', '0x1f62d', '0x1f618', '0x1f60a', '0x1f601', '0x1f629', '0x1f64f',
+    '0x1f60f', '0x1f609', '0x1f64c', '0x1f648', '0x1f604', '0x1f612', '0x1f603', '0x1f614',
+    '0x1f631', '0x1f61c', '0x1f633', '0x1f621', '0x1f60e', '0x1f622', '0x1f60b', '0x1f64a',
+    '0x1f634', '0x1f60c', '0x1f61e', '0x1f606', '0x1f61d', '0x1f62a', '0x1f62b', '0x1f605',
+    '0x1f600', '0x1f61a', '0x1f63b', '0x1f625', '0x1f615', '0x1f624', '0x1f608', '0x1f630',
+    '0x1f611', '0x1f639', '0x1f620', '0x1f613', '0x1f623', '0x1f610', '0x1f628', '0x1f616', 
+    '0x1f637', '0x1f64b', '0x1f61b', '0x1f62c', '0x1f619', '0x1f646', '0x1f645', '0x1f649',
+    '0x1f607', '0x1f63f', '0x1f632', '0x1f636', '0x1f635', '0x1f638', '0x1f627', '0x1f62e',
+    '0x1f63d', '0x1f640', '0x1f647', '0x1f61f', '0x1f62f', '0x1f626', '0x1f64d', '0x1f63a',
+    '0x1f63e', '0x1f63c', '0x1f64e', '0x1f617'
 ]
 dbConnection = sqlite3.connect('datasets/tweets-final.db')
 cursor = dbConnection.execute('CREATE TABLE TWEETS(TWEET_ID VARCHAR2(100), USER_ID VARCHAR2(100), TWEET VARCHAR(400),EMOJI VARCHAR(100),TWEET_SENTIMENT VARCHAR(10))')
@@ -39,20 +39,18 @@ for dbName in dbNames:
     cursor_obj = con.cursor()
     cursor_obj.execute('SELECT * FROM TWEETS')
     rows = cursor_obj.fetchall()
-    count = 0
     for row in rows:
         emoji_list = ['0x{:X}'.format(ord(c)) for c in row[2] if c in emoji.UNICODE_EMOJI['en']]
-        emoji_list = [emo for emo in emoji_list if emo in target_emoji_list]
-        if len(emoji_list) >= 1: 
-            emoji_list_str = '|'.join(emoji_list)
-            cursor.execute("INSERT INTO TWEETS(TWEET_ID, USER_ID, TWEET,EMOJI, TWEET_SENTIMENT) VALUES (?,?,?,?,?)",(row[0],row[1],row[2],emoji_list_str,row[3]))
+        emoji_list = [emo.lower() for emo in emoji_list if emo.lower() in target_emoji_list]
+        if len(emoji_list) >= 1:
+          emoji_list_str = '|'.join(emoji_list)
+          cursor.execute("INSERT INTO TWEETS(TWEET_ID, USER_ID, TWEET,EMOJI, TWEET_SENTIMENT) VALUES (?,?,?,?,?)",(row[0],row[1],row[2],emoji_list_str,row[3]))
     dbConnection.commit()
 print("[INFO] FINISHED SYNCING DATABASES.")
 
 #STEP 1: DATA PRE-PROCESSING
 
 tweets_df = pd.read_sql("SELECT * FROM TWEETS",dbConnection)
-tweets_df.dropna()
 emoticons_df = pd.read_csv("datasets/emoticonsRanked.csv")
 emojis = emoticons_df["Unicode"]
 target_emoticons = emojis.tolist()
@@ -62,7 +60,7 @@ tweets_df.loc[:, 'PROCESSED_TWEET'] = 0
 tweets_df.loc[:, 'TARGET_EMOJIS_LIST'] = 0
 
 process_tweets(tweets_df,target_emoticons)
-tweets_df.to_csv("chec_this.csv")
+
 
 #STEP 2: DERIVE AGGREGATED TEXT SENTIMENT FROM THREE MODELS
 from nltk.sentiment import SentimentIntensityAnalyzer
@@ -84,6 +82,8 @@ tweets_df['emoji_score']=tweets_df['TARGET_EMOJIS_LIST'].apply(lambda x : detect
 #STEP 4: DETERMINE THE DELTA
 tweets_df['delta_nltk']= abs(tweets_df['nltk_aggregated_score'] - tweets_df['emoji_score'])
 tweets_df['delta_spacy']= abs(tweets_df['spacy_aggregated_score'] - tweets_df['emoji_score'])
+
+tweets_df.to_csv("chec_this.csv")
 
 #STEP 5: PLOT THE DELTA VALUES TO DETERMINE THE THRESHOLD
 import matplotlib.pyplot as plt
